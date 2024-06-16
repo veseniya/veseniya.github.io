a1a51e409ba9e2f91ea4cef83e4d21286a26e653
ikryakin <ikryakin@ozon.ru>
2024-06-16 23:28:24 +0300

    prerender error fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    // Disable homepage pre-rendered at build time
+    '/': { prerender: false },
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/ui"]
 })
